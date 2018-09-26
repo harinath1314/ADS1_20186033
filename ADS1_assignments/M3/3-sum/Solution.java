3b303a479507248cad927f1a388d97e72d7dbd3b
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-26 17:31:37 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS1_assignments/M3/3-sum/Solution.java
@@ -0,0 +1,43 @@
+/**.
+ * author : harinatha reddy.
+ * date : 26:9:18
+ * name : threesum probelm.
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * Class for solution of threesum problem.
+ */
+public class Solution {
+    /**
+     * main function is here.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        int arrdim = input.nextInt();
+        int[] threesum = new int[arrdim];
+        for (int i = 0; i < arrdim; i++) {
+            threesum[i] = input.nextInt();
+        }
+        Arrays.sort(threesum);
+        int count = 0;
+        for (int i = 0; i < arrdim; i++) {
+            int j = i + 1;
+            int k = arrdim - 1;
+            if (threesum[i] + threesum[j] + threesum[k] == 0) {
+                count++;
+                j++;
+                k--;
+            } else if (threesum[i] + threesum[j] + threesum[k] < 0) {
+                j++;
+
+            } else {
+                k--;
+            }
+
+        }
+
+    }
+}
