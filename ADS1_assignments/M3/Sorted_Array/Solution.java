b5be7d4953d8b75234ef101455830914037d4556
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-26 22:28:30 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS1_assignments/M3/Sorted_Array/Solution.java
+++ ADS1_assignments/M3/Sorted_Array/Solution.java
@@ -24,7 +24,7 @@ public final class Solution {
 		int k = oner.length - 1;
 		int j = twoer.length - 1;
 		int p = sorted.length - 1;
-		for (int i = p; i > 0; i-- ) {
+		for (int i = p; i >= 0; i-- ) {
 			if (Integer.parseInt(oner[k]) > Integer.parseInt(twoer[j])) {
 				sorted[p] = Integer.parseInt(oner[k]);
 				p--;
