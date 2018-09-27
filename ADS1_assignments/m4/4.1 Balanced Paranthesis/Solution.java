08d797049f706c7abfee372a334032eccb57989d
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-27 18:05:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1_assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1_assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,77 +1,131 @@
+/**
+ * class Solution
+ * autor : c harinatha reddy
+ * date  : 27:9:18
+ */
 import java.util.Arrays;
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * class solution object constructor.
+     */
     private Solution() {
 
     }
-
-    public static void main(String[] args) {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         input.nextLine();
         while (input.hasNext()) {
             char[] brac = input.nextLine().toCharArray();
             System.out.println(isbalanced(brac));
-            
+
         }
 
 
     }
-    public static String isbalanced(char[] brac){
+    /**
+     * is balanced function.
+     *
+     * @param      brac  The brac
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String isbalanced(final char[] brac) {
         Stack s = new Stack();
 
         for (char each : brac) {
-                if (each == '{' || each == '[' || each == '(') {
-                    s.push(each);
-                } else {
-                    if (!s.isempty()) {
-                        char top = s.gettop();
-                        if ((top == '{' && each == '}') || (top == '[' && each == ']') || (top == '(' && each == ')')) {
-                            s.pop();
-                        } else {
-                            return"NO";
-                        }
+            if (each == '{' || each == '[' || each == '(') {
+                s.push(each);
+            } else {
+                if (!s.isempty()) {
+                    char top = s.gettop();
+                    if ((top == '{' && each == '}')
+                        || (top == '[' && each == ']')
+                        || (top == '(' && each == ')')) {
+                        s.pop();
                     } else {
-                        return "NO";
+                        return"NO";
                     }
+                } else {
+                    return "NO";
                 }
             }
-            if (s.isempty()) {
-                return "YES";
-            } else {
-                return "NO";
-            }
+        }
+        if (s.isempty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
 
     }
 }
+/**
+ * List of class.
+ */
 class Stack {
     char [] brackets;
     int size;
+    /**
+     * Constructs the object.
+     */
     public Stack() {
         brackets = new char[10];
         size = 0;
 
     }
-    public void push(char bracket) {
-        if(isfull()){
-            brackets = Arrays.copyOf(brackets,brackets.length*(2));
+    /**
+     * { function_description }
+     *
+     * @param      bracket  The bracket
+     */
+    public void push(final char bracket) {
+        if (isfull()) {
+            brackets = Arrays.copyOf(brackets, brackets.length * (2));
         }
         brackets[size++] = bracket;
 
     }
+    /**
+     * .pop function
+     *
+     * @return     { description_of_the_return_value }
+     */
     public char pop() {
         return brackets[--size];
 
     }
+    /**
+     * is empty function.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public boolean isempty() {
         return size == 0;
     }
+    /**
+     * gettop function.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public char gettop() {
         return brackets[size - 1];
     }
-    public boolean isfull(){
-        return size==brackets.length;
+    /**
+     * isfull function.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean isfull() {
+        return size == brackets.length;
     }
 
 }
