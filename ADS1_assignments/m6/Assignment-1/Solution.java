fe0922c4abd1fc05e5a6d934d3cf7974378c7f2f
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-09-29 11:47:56 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS1_assignments/m6/Assignment-1/Solution.java
+++ ADS1_assignments/m6/Assignment-1/Solution.java
@@ -51,7 +51,7 @@ class AddLargeNumbers {
 	 */
 	static String r ="";
 	public static String digitsToNumber(LinkedList list) {
-			for (Iterator<String> c = list.iterator(); c.hasNext(); System.out.println((c.next())));
+			for (Iterator<String> c = list.iterator(); c.hasNext(); System.out.print((c.next())));
 				return r;
 
 	}
