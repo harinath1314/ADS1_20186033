9243b277369f2b62c86b42213f4886116e434f74
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-10-02 17:05:00 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS1_assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,113 @@
+/**
+ * class for selection method.
+ * author harinatha reddy.
+ * date : 02/10/18.
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * main method starts from here.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Selection[] teams = new Selection[14];
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        while (input.hasNextLine()) {
+            String teamDetails = input.nextLine();
+            teams[i++] = (new Selection(teamDetails));
+        }
+        int min =0;
+        for (int j = 0; j < teams.length; j++) {
+            for (int k = j+1; k < teams.length; k++) {
+                if(teams[k].getwins() < teams[j].getwins()){
+                    Selection temp = teams[j];
+                    teams[j] = teams[k];
+                    teams[k] = temp;
+
+
+                }      
+            }
+        }
+    }
+}
+/**
+ * Class for selection.
+ */
+class Selection {
+    /**
+     * String teamname;.
+     */
+    private String teamname;
+    /**
+     * int wins;.
+     */
+    private int wins;
+    /**
+     * itn loses.
+     */
+    private int loses;
+    /**
+     * int draws.
+     */
+    private int draws;
+    /**
+     * Constructs the object for Selelction class.
+     */
+    Selection(final String data) {
+        String[] details = data.split(",");
+        this.teamname = details[0];
+        this.wins = Integer.parseInt(details[1]);
+        this.loses = Integer.parseInt(details[2]);
+        this.draws = Integer.parseInt(details[3]);
+    }
+
+    /**
+     * getter for teamname.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String getteam() {
+        return teamname;
+    }
+    /**
+     * getter for wins.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int getwins() {
+        return wins;
+    }
+    /**
+     * getter for loses.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int getloses() {
+        return loses;
+    }
+    /**
+     * getter for draws.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int getdraws() {
+        return draws;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public void toString(Selection[] teams){
+        for (int i =0; i<teams.length; i++) {
+            System.out.print(teams[i].getteam()+",");
+        }
+    }
+}
