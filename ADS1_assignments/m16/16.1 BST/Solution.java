5854782550cb3f1a6775fae938045cbfae6e58a0
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-10-11 17:06:58 +0530

    "ads1_m16_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1_assignments/m16/16.1 BST/Solution.java
+++ ADS1_assignments/m16/16.1 BST/Solution.java
@@ -24,7 +24,7 @@ public final class Solution{
 		Bst bst = new Bst();
 		while (input.hasNextLine()) {
 			String[] tokens = input.nextLine().split(",");
-			System.out.println(tokens[0]);
+			// System.out.println(tokens[0]);
 			switch (tokens[0]) {
 			case"put":
 				bst.put((new Book(tokens[1], tokens[2], Double.parseDouble(tokens[3]))) , Integer.parseInt(tokens[2 + 2]));
