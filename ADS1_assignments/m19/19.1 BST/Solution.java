3eb6adb61f0193b3f45ac6ad03d5d3c08c0c6275
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-10-22 18:32:01 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1_assignments/m19/19.1 BST/Solution.java
+++ ADS1_assignments/m19/19.1 BST/Solution.java
@@ -1,83 +1,278 @@
 /**
- * file name :APi using seach tree.
+ * file name :API using search tree.
  * author : harinatha reddy
  * date : 22-10-18.
  */
 
 
+import java.util.Scanner;
 /**
- * solution class is here.
+ * Solution class.
  */
 public final class Solution {
-	/**
-	 * main method for the solution starts here.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
+    /**
+     * Constructs the object for checkstyle.
+     */
+    private Solution() {
 
-	}
-}
-/**
+    }
+    /**
+     * main method starts here.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        Bst bst = new Bst();
+        while (input.hasNext()) {
+            String[] tokens = input.nextLine().split(",");
+
+            switch (tokens[0]) {
+            case"put":
+                bst.put(new Book(tokens[1], tokens[2],
+                                 Double.parseDouble(tokens[2 + 1])),
+                        Integer.parseInt(tokens[2 + 2]));
+                break;
+            case"get":
+                System.out.println(bst.get((new Book(tokens[1],
+                tokens[2], Double.parseDouble(tokens[2 + 1])))));
+                break;
+            default:
+                break;
+            }
+
+        }
+
+
+    }
+
+}/**
  * Class for book.
  */
 class Book {
-	/**
-	 * String varible bok name.
-	 */
-	private String bookname;
-	/**
-	 * String variable author.
-	 */
-	private String bookauthor;
-	/**
-	 * cost of the book .
-	 */
-	private Double price;
-	/**
-	 * Constructs the object for book class.
-	 *
-	 * @param      name    The name
-	 * @param      author  The author
-	 * @param      cost    The cost
-	 */
-	Book(String name, String author, Double cost) {
-		this.bookname = name;
-		this.bookauthor = author;
-		this.price = cost;
+    /**
+     * book name.
+     */
+    private String name;
+    /**
+     * book author.
+     */
+    private String author;
+    /**
+     * book price.
+     */
+    private double price;
+
+    /**
+     * Constructs the  for Book class.
+     *
+     * @param      bookname    The bookname
+     * @param      authorname  The authorname
+     * @param      bookprice   The bookprice
+     */
+    Book(final String bookname, final String authorname,
+         final double bookprice) {
+        this.name = bookname;
+        this.author = authorname;
+        this.price = bookprice;
+    }
+    /**
+     * getter for name.
+     *
+     * @return      name of book.
+     */
+    public String getname() {
+        return name;
+    }
+    /**
+     * getter for author.
+     *
+     * @return      author name.
+     */
+    public String getauthor() {
+        return author;
+    }
+    /**
+     * getter for price.
+     *
+     *
+     * @return     price of book.
+     */
+    public double getprice() {
+        return price;
+    }
+    /**
+     * compare to funtcion.
+     *
+     * @param      that  The that.
+     *
+     * @return     integer.
+     */
+    public int compareTo(final Book that) {
+
+        if (this.getname().compareTo(that.getname()) > 0) {
+            return 1;
+        } else if (this.getname().compareTo(that.getname()) < 0) {
+            return -1;
+        } else {
+            if (this.getauthor().compareTo(that.getauthor()) > 0) {
+                return 1;
+            } else if (this.getauthor().compareTo(that.getauthor()) < 0) {
+                return -1;
+            } else {
+                if (this.getprice() > that.getprice()) {
+                    return 1;
+                } else if (this.getprice() < that.getprice()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            } 
+        }
+    }
 
 
-	}
-	/**
-	 * getter for the book name variable.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public String getname() {
-		return bookname;
-	}
 
 }
 
 
-// /**
-//  * Class for bst.
-//  */
-// class BST{
-// 	/**
-// 	 * Class for node.
-// 	 */
-// 	private class Node{
-// 		private Key key;
-// 		private Value val;
-// 		private Node left;
-// 		private Node right;
-// 		private int  count;
+/**
+ * Class for bst.
+ */
+class Bst{
+	/**
+	 * Class for node.
+	 */
+	private class Node{
+		private Book key;
+		private Integer val;
+		private Node left;
+		private Node right;
+		private int  count;
+		/**
+		 * Constructs the object.
+		 *
+		 * @param      book   The book
+		 * @param      value  The value
+		 */
+		Node(Book book, Integer value){
+			this.key = book;
+			this.val = value;
+			this.left = null;
+			this.right = null;
 
-// 		Node(Key book, Value val){
-			
-// 		}
 
 
-// 	}
-// }
+		}
+
+
+	}
+
+	/**
+     * root.
+     */
+    private Node root;
+    /**
+     * Constructs the object.
+     */
+    Bst() {
+        this.root = null;
+    }
+    /**
+     * size method.
+     *
+     * @return     { description_of_the_return_value }
+     */
+	public int size(){
+		return size(root);
+	}
+	/**
+	 * size with root.
+	 *
+	 * @param      x     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public int size(Node x){
+		if (x == null){
+			return 0;
+		}
+		return x.count;
+	}
+	/**
+     * put method.
+     * complexity of put method in average is log N
+     * complexity of put method in worst case is N.
+     *
+     * @param      book    The book
+     * @param      volume  The volume
+     */
+    public void put(final Book book, final Integer volume) {
+        root = helpingnature(root, book, volume);
+
+    }
+	/**
+     * helper function.
+     *
+     * @param      x       { parameter_description }
+     * @param      book    The book
+     * @param      volume  The volume
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Node helpingnature(final Node x, final Book book,
+                               final Integer volume) {
+
+        if (x == null) {
+            return new Node(book, volume);
+        }
+        int cmp = book.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = helpingnature(x.left, book, volume);
+
+        } else if (cmp > 0) {
+            x.right = helpingnature(x.right, book, volume);
+
+        } else {
+            x.val = volume;
+        }
+        return x;
+
+    }
+    /**
+     * get method.
+     * complexity of get method in average is log N
+     * complexity of get method in worst case is N.
+     *
+     *
+     * @param      book  The book
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Integer get(final Book book) {
+        return (gethelp(root, book));
+
+    }
+    /**
+     * gethelp.
+     *
+     * @param      x     { parameter_description }
+     * @param      book  The book
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Integer gethelp(final Node x, final Book book) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = book.compareTo(x.key);
+        if (cmp < 0) {
+           return  gethelp(x.left, book);
+
+        } else if (cmp > 0) {
+           return  gethelp(x.right, book);
+
+        }
+        return x.val;
+    }
+}
